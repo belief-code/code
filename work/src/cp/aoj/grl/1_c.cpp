6e5d70c61264fea685f702d5b1b02fcc09e5d45b
belief-code <hasebeyudai.code@gmail.com>
2021-07-15 07:27:50 +0900

    /07/15

---
+++ work/src/cp/aoj/grl/1_c.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define rrep(i,n) for(int i = 1; i <= (n); ++i)
+#define drep(i,n) for(int i = (n)-1; i >= 0; --i)
+#define srep(i,s,t) for(int i=s;i<(t);++i)
+#define rng(a) a.begin(),a.end()
+#define rrng(a) a.rbegin(),a.rend()
+#define isin(x,l,r) ((l) <= (x) && (x) < (r))
+#define pb push_back
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+#define pcnt __builtin_popcountll
+#define bn(x) ((1<<(x))-1)
+using namespace std;
+template<class T> using vc = vector<T>;
+template<class T> using vv = vc<vc<T>>;
+using ll = long long;
+using uint = unsigned;
+using ull = unsigned long long;
+using P = pair<int,int>;
+using T3 = tuple<int,int,int>;
+using vi = vc<int>;
+using vvi = vv<int>;
+using vl = vc<ll>;
+using vp = vc<P>;
+using vt = vc<T3>;
+int getInt(){int x;scanf("%d",&x);return x;}
+template<typename T>T dup(T x, T y){return (x+y-1)/y;}
+template<typename T>void uni(T& a){sort(rng(a));a.erase(unique(rng(a)),a.end());}
+const double eps = 1e-10;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+
+
+int main(){
+    return 0;
+}
+
+
+/*
+全点対間最短経路の距離を全列挙する必要ある。
+まず最初にこれらを一つ一つ個別に求めていくことを考える。
+これを時間内に達成するためには
+
+*/
